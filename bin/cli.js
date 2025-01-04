827724423a42db00c9fdf7bc4b183da78e630abc
Marcos Iván Alvarez <alvmivan@gmail.com>
2025-01-04 17:57:09 -0300

    init

---
+++ bin/cli.js
@@ -0,0 +1,21 @@
+﻿import {program} from "commander";
+
+import {init, install, update} from "../lib";
+
+
+program
+    .command('init')
+    .description('Create an inner-packages.json file if it does not exist.')
+    .action(init);
+
+program
+    .command('install [repo]')
+    .description('Install packages from inner-packages.json or a specific repository URL.')
+    .action(install);
+
+program
+    .command('update')
+    .description('Update all installed packages.')
+    .action(update);
+
+program.parse(process.argv);
